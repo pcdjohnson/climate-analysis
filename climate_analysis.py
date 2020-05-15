35bdd8447f403a553a2539d74b1611279d1d089b
Paul Johnson <pcdjohnson@gmail.com>
2020-05-15 14:14:59 +0100

    Add DocString, delete 2 empty lines

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,12 +1,12 @@
+""" Climate Analysis Tools """
+
 import sys
 import temp_conversion
 import signal
 signal.signal(signal.SIGPIPE, signal.SIG_DFL)
-
 script = sys.argv[0]
 assert len(sys.argv) == 2, script + ": requires filename"
 filename = sys.argv[1]
-
 climate_data = open(filename, 'r')
 
 for line in climate_data:
